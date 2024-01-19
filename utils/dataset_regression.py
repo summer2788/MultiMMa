0323c1dd7e0f92c2d8e533394f386a7f473c5bf2
smori <summer2788@g.skku.edu>
2024-01-20 04:05:49 +0900

    mask valid case if None

--- utils/dataset_regression.py
+++ utils/dataset_regression.py
@@ -119,8 +119,8 @@ class DataAugmentationForRegression(object):
         #if task_dict['mask_valid'] == None:
         if 'mask_valid' not in task_dict:
              task_dict['mask_valid'] = torch.ones_like(task_dict['depth'])
-        else:   
-            task_dict['mask_valid'] = (task_dict['mask_valid'] == 255)[None] # This is needed because the mask is 0-255 and we need 0-1
+         
+        task_dict['mask_valid'] = (task_dict['mask_valid'] == 255)[None] # This is needed because the mask is 0-255 and we need 0-1
 
 
         for task in task_dict:
